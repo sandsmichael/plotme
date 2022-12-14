3754b05d7cd5a393ef893f9f28bf223dcc629f99
Michael Sands <michaelesands@gmail.com>
2022-12-13 22:48:14 -0500

    grouped plots

--- plot.py
+++ plot.py
@@ -5,57 +5,18 @@
   └────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
  """
 import pandas as pd
+from pandas.api.types import is_datetime64_any_dtype
 import numpy as np
-
 import matplotlib.pyplot as plt 
-
+import matplotlib.dates as mdates
 import seaborn as sns
-sns.set_theme(style="whitegrid")
-
-
 from sqlalchemy import create_engine
 
-# Build dummy data
-# engine = create_engine('sqlite:///C:\data\industry_fundamentals.db', echo=False)
-# cnxn = engine.connect()
-# df = pd.read_sql("select * from CompFunBase", cnxn)[['ticker','name','calendardate','sector','industry','revenue', 'fcfmargin','eps','oppmargin','profitmargin', 'netmargin', 'pe','roc', 'roe',]].dropna(axis=0)
-# df = df.sort_values(by=['sector','industry','revenue'], ascending = False)
-# industry_leaders = df.drop_duplicates(subset=['industry'], keep = 'first')
-# df = df[df.ticker.isin(industry_leaders.ticker)]
-# df.to_csv('./equity_fundamentals_revenue_leaders.csv')
-# print(df)
+sns.set_theme(style="whitegrid")
+sns.set_palette("Set2")
 
-df = pd.read_csv('./equity_fundamentals_revenue_leaders.csv')
 
-#       ticker                              name calendardate           sector                     industry       revenue  fcfmargin   eps  oppmargin  profitmargin  netmargin       pe       roc       roe
-# 21383    CEG         CONSTELLATION ENERGY CORP   2022-09-30        Utilities        Utilities - Renewable  6.051000e+09  -0.245249 -0.57  -0.006776     -0.031069   0.036688 -180.068 -0.011651 -0.017436
-# 21381    CEG         CONSTELLATION ENERGY CORP   2022-03-31        Utilities        Utilities - Renewable  5.591000e+09   0.168306  0.32   0.077804      0.018959   0.125022   26.477  0.006255  0.009526
-# 21382    CEG         CONSTELLATION ENERGY CORP   2022-06-30        Utilities        Utilities - Renewable  5.465000e+09  -0.087466 -0.34   0.049771     -0.020311   0.100823   29.048 -0.006988 -0.010097
-# 11863    AWK  AMERICAN WATER WORKS COMPANY INC   2021-09-30        Utilities  Utilities - Regulated Water  1.092000e+09   0.026557  1.53   0.453297      0.254579   0.600733   40.210  0.015418  0.040489
-# 11884    AWK  AMERICAN WATER WORKS COMPANY INC   2022-09-30        Utilities  Utilities - Regulated Water  1.082000e+09  -0.064695  1.63   0.463956      0.274492   0.615527   17.952  0.015093  0.038283
-# ...      ...                               ...          ...              ...                          ...           ...        ...   ...        ...           ...        ...      ...       ...       ...
-# 77683    NTR                       NUTRIEN LTD   2018-09-30  Basic Materials          Agricultural Inputs  3.990000e+09  -0.132581 -1.70  -0.364912     -0.261654   0.289474  123.331 -0.029491 -0.047379
-# 77684    NTR                       NUTRIEN LTD   2018-12-31  Basic Materials          Agricultural Inputs  3.875000e+09   0.354581  5.13   0.140387      0.826323   0.324903    8.089  0.095162  0.131095
-# 77685    NTR                       NUTRIEN LTD   2019-03-31  Basic Materials          Agricultural Inputs  3.719000e+09  -0.226674  0.07   0.050282      0.011024   0.262167    8.881  0.001190  0.001730
-# 77681    NTR                       NUTRIEN LTD   2018-03-31  Basic Materials          Agricultural Inputs  3.666000e+09  -0.157665  0.00   0.040371     -0.000273   0.231042  171.885 -0.000029 -0.000043
-# 77688    NTR                       NUTRIEN LTD   2019-12-31  Basic Materials          Agricultural Inputs  3.503000e+09   0.520411 -0.08   0.043677     -0.013703   0.306880   27.668 -0.001413 -0.002099
 
-melt = df.melt(id_vars = ['ticker', 'name', 'calendardate', 'sector', 'industry'], value_vars = ['fcfmargin','eps','oppmargin','profitmargin', 'netmargin', 'roc', 'roe'])
-
-#       ticker                       name calendardate           sector               industry variable         value
-# 0        CEG  CONSTELLATION ENERGY CORP   2022-09-30        Utilities  Utilities - Renewable  revenue  6.051000e+09
-# 1        CEG  CONSTELLATION ENERGY CORP   2022-03-31        Utilities  Utilities - Renewable  revenue  5.591000e+09
-# 2        CEG  CONSTELLATION ENERGY CORP   2021-03-31        Utilities  Utilities - Renewable  revenue  5.559000e+09
-# 3        CEG  CONSTELLATION ENERGY CORP   2021-12-31        Utilities  Utilities - Renewable  revenue  5.532000e+09
-# 4        CEG  CONSTELLATION ENERGY CORP   2022-06-30        Utilities  Utilities - Renewable  revenue  5.465000e+09
-# ...      ...                        ...          ...              ...                    ...      ...           ...
-# 12843    NTR                NUTRIEN LTD   2019-12-31  Basic Materials    Agricultural Inputs      rnd  0.000000e+00
-# 12844    NTR                NUTRIEN LTD   2017-09-30  Basic Materials    Agricultural Inputs      rnd  0.000000e+00
-# 12845    NTR                NUTRIEN LTD   2017-06-30  Basic Materials    Agricultural Inputs      rnd  0.000000e+00
-# 12846    NTR                NUTRIEN LTD   2017-03-31  Basic Materials    Agricultural Inputs      rnd  0.000000e+00
-# 12847    NTR                NUTRIEN LTD   2017-12-31  Basic Materials    Agricultural Inputs      rnd  0.000000e+00
-
-# print(melt)
 class Yax:
 
     def __init__(self, g):
@@ -74,16 +35,35 @@ class Yax:
         self.g.set_yticklabels(ylabels)
 
 
-    def set_ylim(self, data):
-        # calculate ylim based on data param
+    def set_ylim(self, lower, upper):
+
+        plt.ylim([lower, upper]) # NOTE: Must specify ylim values before calling set_yabels
+
+
+    def set_yticks(self, data):
         
-        plt.ylim([-1, 1])
+        y = np.array([0.650, 0.660, 0.675, 0.685]) # TODO: calculate best 
+        
+        plt.yticks(np.arange(y.min(), y.max(), 0.005))
+
+
+
+class Xax:
+
+    def __init__(self, g):
+
+        self.g = g
+
+    def rotate(self, degrees):
+        
+        plt.xticks(rotation = degrees, ha = 'center')
+
 
 
 
 class Plotme:
 
-    def __init__(self,  data, x, y, figsize=(8, 5)):
+    def __init__(self,  data, x, y, hue=None, fig=None, figsize=(20, 10)):
 
         self.data = data
 
@@ -91,50 +71,174 @@ class Plotme:
 
         self.y = y
 
-        self.figsize = figsize
-        
+        self.hue = hue
 
-    def box_swarm_plot(
-                    self, 
-                    title, 
-                    showfliers = False,
-                    orient = 'v'
-                ):
-        
-        fig = plt.figure(figsize=self.figsize, dpi=80)
-        
-        # ax = fig.add_subplot(111)
+        self.fig = fig
 
-        g = sns.boxplot(data=self.data, x = self.x, y = self.y, palette="Set2", showfliers = showfliers, orient = orient)
+        if self.fig is not None:
+            
+            self.fig = plt.figure(figsize=self.figsize, dpi=80)
+
+        self.figsize = figsize 
+
+
+
+    def box_swarm(
+        self, 
+        title, 
+        showfliers = False,
+        orient = 'v'
+    ):
+        
+        g = sns.boxplot(data=self.data, x = self.x, y = self.y, showfliers = showfliers, orient = orient)
 
         sns.swarmplot(data=self.data, x = self.x, y = self.y, color="black", size = 2 )
 
         g.set_title(title)
 
-        axis = Yax(g)
+        yax = Yax(g)
 
-        axis.set_ylim(self.data)
+        yax.set_ylabels(units=None)
 
-        axis.set_ylabels(units=None)
+        plt.show()
+
+        return g
+
+    
+
+    def clustered_bar(
+        self, 
+        title:str,
+        label_annotation:str = None,
+    ):
+        """[Summary]
+        
+        :param [label_annotation]: 
+            Melted dataframe must have an equal number of records across the ploted bars in order to annotate
+        
+        :raises [ErrorType]: 
+        """    
+
+        g = sns.barplot(data = self.data, x = self.x, y = self.y, hue = self.hue, palette="Set2")
+
+        if label_annotation is not None:
+            
+            patch_labels = []
+            for i in range(len(self.data[label_annotation])): # data must have the same number of records for each annotation
+                patch_labels.append(self.data[label_annotation].iloc[i]) 
+
+            # annotate bar centering around half of it's height adjusting for the lenght of the text being written
+            for i, p in enumerate(g.patches):  
+                    g.annotate( patch_labels[i], (p.get_x() + (p.get_width() / 2.0), (p.get_height() - ( p.get_height() * (len(patch_labels[i]) + 1) / 100 ) )/ 2 ), ha = 'center', va = 'center', rotation = 90, size = 10 ) #  xytext = (1, 0), textcoords = 'offset points',
+
+            g.get_legend().remove()
+
+        g.set_title(title)
+
+        yax = Yax(g)
+
+        yax.set_ylabels(units = 'M')
+
+        xax = Xax(g)
+
+        xax.rotate(degrees = 45)
 
         plt.show()
 
         return g
 
 
-p = Plotme( data = melt[melt.calendardate == '2022-09-30'][-200:], x = 'variable', y = 'value')
-p.box_swarm_plot(title = 'Boxplot')
+
+    def facet_grid(
+        self, 
+        title:str,
+        col,
+        row,
+        map,
+        height = 5,
+        sharex = False,
+        sharey = False,
+        despine = False,
+        palette = "Set2"
+    ):
+
+        g = sns.FacetGrid(data = self.data, col = col, row = row, hue = None, height = height, sharex=sharex, sharey=sharey, despine=despine)
+
+        if map == 'line':
+            g.map(sns.lineplot,self.x, self.y, palette=palette)
+        
+        if map == 'bar':
+            g.map(sns.barplot, self.x, self.y, ci = None, palette=palette)
+
+        for ax in g.axes.flat:
+
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='center')
+            
+            # fmt = mdates.DateFormatter('%Y-%m-%d')
+            
+            # ax.xaxis.set_major_formatter(fmt)
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y'))
+            
 
 
 
-# def clustered_bar():
-#   # Clustered bar with horizontal labels; 
-#   pass
+
+        g.set_titles(col_template = '{col_name}', row_template='{row_name}')
+
+        color_palette = sns.color_palette(palette, 20)
+
+        plt.show()
+
+        return g
 
 
-# def facet_grid():
-#   # Facetgrid (param: melted) color coded axis labels/bars;
-#   pass
+""" 
+  ┌──────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Example:                                                                                                         │
+  └──────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+ """
+
+df = pd.read_csv('./equity_fundamentals_revenue_leaders.csv')
+melt = df.melt(id_vars = ['ticker', 'name', 'calendardate', 'sector', 'industry'], value_vars = ['fcfmargin','eps','oppmargin','profitmargin', 'netmargin', 'roc', 'roe', 'revenue'])
+
+# ...Sample Data...
+melt.calendardate = pd.to_datetime(melt.calendardate)
+
+sample1 = melt[melt.calendardate == '2022-09-30'][-200:]
+
+sample2 = melt[ (melt.sector == 'Utilities') & (melt.calendardate > pd.to_datetime('2020-09-30')) & (melt.variable == 'revenue') ]
+sample2 = sample2[sample2.ticker != 'CEG']
+sample2 = sample2.sort_values(by = ['ticker', 'calendardate'], ascending=True).reset_index(drop=True)
+
+sample3 = melt[ (melt.sector == 'Utilities') & (melt.calendardate > pd.to_datetime('2015-03-31')) & (melt.variable.isin(['fcfmargin','oppmargin','profitmargin', 'netmargin'])) ]
+sample3 = sample3[sample3.ticker != 'CEG']
+sample3 = sample3.sort_values(by = ['ticker', 'calendardate'], ascending=True).reset_index(drop=True)
+print(sample3.dtypes)
+print(sample3)
+
+# ...Examples...
+
+# Boxplot
+# p = Plotme( data = sample_boxplot, x = 'variable', y = 'value')
+# p.box_swarm(title = 'Boxplot')
+
+
+# Clustered Bar Plot
+# p = Plotme( data = sample_clustered_bar, x = 'calendardate', y = 'value', hue = 'name')
+# p.clustered_bar(title = 'Clustered Bar', label_annotation = 'ticker')
+
+
+# Facet Grid Line
+# p = Plotme( data = sample3, x = 'calendardate', y = 'value')
+# p.facet_grid(col='variable', row = 'name', title = 'Facet Grid of Line Charts')
+
+
+# Facet Grid Bar
+p = Plotme( data = sample3, x = 'calendardate', y = 'value', hue = 'variable')
+p.facet_grid(col = 'name', row = None, map = 'bar', title = 'Facet Grid of Line Charts')
+
+
+
 
 
 # def waterfall():
